9aaf1232f9b312f82b3ba03fa72440f5f3e3c75f
Tim Berger <timmberg99@gmail.com>
2022-05-16 11:18:45 +0200

    servocontrol

--- V02/servo_ctrl.py
+++ V02/servo_ctrl.py
@@ -1,32 +1,52 @@
 #!/usr/bin/env python
 
 #######################################################################
-#                            Aufgabe 1.3                              #
+#                            Aufgabe 1.4                              #
 #######################################################################
 
 import gpio_class
 
+
 def write(servo, pulse):
     gpio_class.write(servo, pulse)
 
+
 class Motor(object):
-    PWM_PIN = 1     # GPIO pin 11
+    PWM_PIN = 1  # GPIO pin 11
     min_pulse = 100
     max_pulse = 200
+
     def __init__(self, servo=None):
-        return
-    def set_speed(self, speed):
-        return
-    def stop(self):
+        self.servo = servo
         return
 
+    def set_speed(self, speed):
+        if speed == -11:
+            write(self.servo, 100)
+        elif speed == 11:
+            write(self.servo, 200)
+        else:
+            write(self.servo, 150)
+
+    def stop(self):
+        write(self.servo, 150)
+
+
 class Steering(object):
-    PWM_PIN = 2     # GPIO pin 12
+    PWM_PIN = 2  # GPIO pin 12
     min_pulse = 100
     max_pulse = 200
+
     def __init__(self, servo=None):
-        return
+        self.servo = servo
+
     def set_angle(self, angle):
-        return
+        if angle == -45:
+            write(self.servo, 115)
+        elif angle == 45:
+            write(self.servo, 195)
+        else:
+            write(self.servo, 155)
+
     def stop(self):
-        return
+        write(self.servo, 155)
