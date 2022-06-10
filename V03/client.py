fd486d638e91dd2dcc226f1d05c73aff16d27826
selimlouis <selimseyhanoglu@gmail.com>
2022-06-11 01:50:38 +0200

    add subprocesses

--- V03/client.py
+++ V03/client.py
@@ -1,10 +1,16 @@
+from pickle import TRUE
 import socket
 from time import sleep
 from common import address_families, socket_types
+import subprocess
 
 # IP und Port des Servers
 IP = '192.168.0.11'
 PORT = 8000
+BUFFER_SIZE = 1400
+DUMMY = True
+TCP = True
+UDP = not TCP
 
 # Passenden Address- und Sockettyp w\"ahlen
 address_family = address_families[0]
@@ -13,11 +19,27 @@ socket_type = socket_types[0]
 sock = socket.socket(address_family, socket_type)
 # Verbinden zu einem Server-Socket (Nur TCP)
 sock.connect((IP,PORT))
+
+
 # Sende immer wieder "Hello" an den Server
-while True:
-    message = b"Hello\n"
-    # TCP
-    sock.send(message)
-    # UDP
-    sock.sendto(message, (IP, PORT))
-    sleep(1)
+if DUMMY:
+    while True:
+        message = b"Hello\n"
+        if TCP:
+            sock.send(message)
+        elif UDP:
+            sock.sendto(message, (IP, PORT))
+            sleep(1)
+else:
+    # tatsächliches video streaming
+    time = 0
+    fps = 20
+    bitrate = 2000000
+    cmd_raspivid = f'raspivid -t {time} -fps {fps} -w 1280 -h 720 -b {bitrate} -o -'
+    rasprocess = subprocess.Popen(cmd_raspivid,shell=True,stdout=subprocess.PIPE)
+    while True:
+        data = rasprocess.stdout.read(BUFFER_SIZE)
+        if TCP:
+            sock.send(data)
+        elif UDP:
+            sock.sendto(data, (IP, PORT))
--- V03/server.py
+++ V03/server.py
@@ -1,9 +1,13 @@
 import socket
 from common import address_families, socket_types
+import subprocess
 # Port des Servers
 PORT = 8000
 # Lesepuffergr\"o\ss e
 BUFFER_SIZE = 1400
+DUMMY = True
+TCP = True
+UDP = not TCP
 
 # Passenden Address- und Sockettyp w\"ahlen
 address_family = address_families[0]
@@ -14,15 +18,28 @@ backlog = 1
 sock = socket.socket(address_family, socket_type)
 sock.bind(('192.168.0.11', PORT))
 
-
 # Lausche am Socket auf eingehende Verbindungen (Nur TCP)
 sock.listen(backlog)
 clientsocket, address = sock.accept()
-# Daten (der Gr\"o\ss e BUFFER_SIZE) aus dem Socket holen und ausgeben:
-while True:
-    # TCP:
-    data = clientsocket.recv(BUFFER_SIZE)
-    print(data)
-    # UDP:
-    # data, address = sock.recvfrom(BUFFER_SIZE)
-    # print(data)
+
+if DUMMY:
+    # Daten (der Gr\"o\ss e BUFFER_SIZE) aus dem Socket holen und ausgeben:
+    while True:
+        if TCP:
+            data = clientsocket.recv(BUFFER_SIZE)
+            print(data)
+        elif UDP:
+            data, address = sock.recvfrom(BUFFER_SIZE)
+            print(data)
+else:
+    # mplayer starten
+    cmd_mplayer = 'mplayer -fps 25 -cache 512 -'
+    mprocess = subprocess.Popen(cmd_mplayer, shell=True, stdin=subprocess.PIPE)
+
+    while True:
+        if TCP:
+            data = clientsocket.recv(BUFFER_SIZE)
+            mprocess.stdin.write(data)
+        elif UDP:
+            data, address = sock.recvfrom(BUFFER_SIZE)
+            mprocess.stdin.write(data)
