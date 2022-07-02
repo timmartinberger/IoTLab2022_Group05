922fe8354eea07c5efe7453947afbf0cfea43d33
Tim Berger <timmberg99@gmail.com>
2022-07-02 11:00:38 +0200

    start V4

---
+++ V04/ikt_car_sensorik.py
@@ -0,0 +1,258 @@
+#!/usr/bin/python
+import os
+from time import time, sleep
+import threading
+import RPi.GPIO as GPIO
+import smbus
+
+GPIO.setmode(GPIO.BCM)
+
+# 1 indicates /dev/i2c-1 (port I2C1)
+bus = smbus.SMBus(1)
+
+
+#################################################################################
+# Sensors
+#################################################################################
+
+#################################################################################
+# Ultrasonic
+#################################################################################
+
+class Ultrasonic():
+    '''This class is responsible for handling i2c requests to an ultrasonic sensor'''
+
+    def __init__(self, address):
+        self.address = address
+
+    # Aufgabe 2
+    #
+    # Diese Methode soll ein Datenbyte an den Ultraschallsensor senden um eine Messung zu starten
+    def write(self, value):
+        return 0
+
+    # Aufgabe 2
+    #
+    # Diese Methode soll den Lichtwert auslesen und zurueckgeben.
+    def get_brightness(self):
+        return 0
+
+    # Aufgabe 2
+    #
+    # Diese Methode soll die Entfernung auslesen und zurueckgeben.
+    def get_distance(self):
+        return 0
+
+    def get_address(self):
+        return self.address
+
+
+class UltrasonicThread(threading.Thread):
+    ''' Thread-class for holding ultrasonic data '''
+
+    # distance to obstacle in cm
+    distance = 0
+
+    # brightness value
+    brightness = 0
+
+    # Aufgabe 4
+    #
+    # Hier muss der Thread initialisiert werden.
+    def __init__(self, address):
+        return 0
+
+    # Aufgabe 4
+    #
+    # Schreiben Sie die Messwerte in die lokalen Variablen
+    def run(self):
+        while not self.stopped:
+            continue
+
+    def stop(self):
+        self.stopped = True
+
+
+#################################################################################
+# Compass
+#################################################################################
+
+class Compass(object):
+    '''This class is responsible for handling i2c requests to a compass sensor'''
+
+    def __init__(self, address):
+        self.address = address
+
+    # Aufgabe 2
+    #
+    # Diese Methode soll den Kompasswert auslesen und zurueckgeben.
+    def get_bearing(self):
+        return 0
+
+
+class CompassThread(threading.Thread):
+    ''' Thread-class for holding compass data '''
+
+    # Compass bearing value
+    bearing = 0
+
+    # Aufgabe 4
+    #
+    # Hier muss der Thread initialisiert werden.
+    def __init__(self, address):
+        return 0
+
+    # Aufgabe 4
+    #
+    # Diese Methode soll den Kompasswert aktuell halten.
+    def run(self):
+        while not self.stopped:
+            continue
+
+    def stop(self):
+        self.stopped = True
+
+
+#################################################################################
+# Infrared
+#################################################################################
+
+class Infrared(object):
+    '''This class is responsible for handling i2c requests to an infrared sensor'''
+
+    def __init__(self, address):
+        self.address = address
+
+    # Aufgabe 2
+    #
+    # In dieser Methode soll der gemessene Spannungswert des Infrarotsensors ausgelesen werden.
+    def get_voltage(self):
+        return 0
+
+    # Aufgabe 3
+    #
+    # Der Spannungswert soll in einen Distanzwert umgerechnet werden.
+    def get_distance(self):
+        return 0
+
+
+class InfraredThread(threading.Thread):
+    ''' Thread-class for holding Infrared data '''
+
+    # distance to an obstacle in cm
+    distance = 0
+
+    # length of parking space in cm
+    parking_space_length = 0
+
+    # Aufgabe 4
+    #
+    # Hier muss der Thread initialisiert werden.
+    def __init__(self, address, encoder=None):
+        return 0
+
+    def run(self):
+        while not self.stopped:
+            read_infrared_value()
+            calculate_parking_space_length()
+
+    # Aufgabe 4
+    #
+    # Diese Methode soll den Infrarotwert aktuell halten
+    def read_infrared_value(self):
+        return 0
+
+    # Aufgabe 5
+    #
+    # Hier soll die Berechnung der Laenge der Parkluecke definiert werden
+    def calculate_parking_space_length(self):
+        return 0
+
+    def stop(self):
+        self.stopped = True
+
+
+#################################################################################
+# Encoder
+#################################################################################
+
+class Encoder(object):
+    ''' This class is responsible for handling encoder data '''
+
+    # Aufgabe 2
+    #
+    # Wieviel cm betraegt ein einzelner Encoder-Schritt?
+    STEP_LENGTH = 0  # in cm
+
+    # number of encoder steps
+    count = 0
+
+    def __init__(self, pin):
+        self.pin = pin
+
+    # Aufgabe 2
+    #
+    # Jeder Flankenwechsel muss zur Berechnung der Entfernung gezaehlt werden.
+    # Definieren Sie alle dazu noetigen Methoden.
+
+    # Aufgabe 2
+    #
+    # Diese Methode soll die gesamte zurueckgelegte Distanz zurueckgeben.
+    def get_travelled_dist(self):
+        return 0
+
+
+class EncoderThread(threading.Thread):
+    ''' Thread-class for holding speed and distance data of all encoders'''
+
+    # current speed.
+    speed = 0
+
+    # currently traversed distance.
+    distance = 0
+
+    # Aufgabe 4
+    #
+    # Hier muss der Thread initialisiert werden.
+    def __init__(self, encoder):
+        return 0
+
+    def run(self):
+        while not self.stopped:
+            get_values()
+
+    # Aufgabe 4
+    #
+    # Diese Methode soll die aktuelle Geschwindigkeit sowie die zurueckgelegte Distanz aktuell halten.
+    def get_values(self):
+        return 0
+
+    def stop(self):
+        self.stopped = True
+
+
+#################################################################################
+# Main Thread
+#################################################################################	
+
+if __name__ == "__main__":
+    # The GPIO pin to which the encoder is connected
+    encoder_pin = 23
+
+    # Aufgabe 1
+    #
+    # Tragen Sie die i2c Adressen der Sensoren hier ein
+
+    # The i2c addresses of front and rear ultrasound sensors
+    ultrasonic_front_i2c_address = 0x00;
+    ultrasonic_rear_i2c_address = 0x00;
+
+    # The i2c address of the compass sensor
+    compass_i2c_address = 0x00
+
+    # The i2c address of the infrared sensor
+    infrared_i2c_address = 0x00
+
+# Aufgabe 6
+#
+# Hier sollen saemtlichen Messwerte periodisch auf der Konsole ausgegeben werden.
---
+++ V04/ikt_car_webserver.py
@@ -0,0 +1,93 @@
+﻿#!/usr/bin/python
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import io
+import json
+
+import threading
+from ikt_car_sensorik import *
+import _servo_ctrl
+from math import acos, sqrt, degrees
+
+
+# Aufgabe 4
+#
+# Der Tornado Webserver soll die Datei index.html am Port 8081 zur Verfügung stellen
+
+# Aufgabe 3
+#
+# Der Tornado Webserver muss eine Liste der clients verwalten.  
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    '''Definition der Operationen des WebSocket Servers'''
+
+    print "hello WebSocketHandler"
+
+    def open(self):
+        return 0
+
+    def on_message(self, message):
+        return 0
+
+    def on_close(self):
+        return 0
+
+
+class DataThread(threading.Thread):
+    '''Thread zum Senden der Zustandsdaten an alle Clients aus der Client-Liste'''
+
+    # Aufgabe 3
+    #
+    # Hier muss der Thread initialisiert werden.
+    def __init__(self):
+        return 0
+
+    # Aufgabe 3
+    #
+    # Erstellen Sie hier Instanzen von Klassen aus dem ersten Teilversuch
+    def set_sensorik(self, address_ultrasonic_front, address_ultrasonic_back, address_compass, address_infrared,
+                     encoder_pin):
+        return 0
+
+    # Aufgabe 3
+    #
+    # Hier muessen die Sensorwerte ausgelesen und an alle Clients des Webservers verschickt werden.
+    def run(self):
+        while not self.stopped:
+            continue
+
+    def stop(self):
+        self.stopped = True
+
+
+class DrivingThread(threading.Thread):
+    '''Thread zum Fahren des Autos'''
+
+    # Einparken
+    #
+    # Hier muss der Thread initialisiert werden.
+    def __init__(self):
+        return 0
+
+    # Einparken
+    #
+    # Definieren Sie einen Thread, der auf die ueber den Webserver erhaltenen Befehle reagiert und den Einparkprozess durchfuehrt
+    def run(self):
+        while not self.stopped:
+            continue
+
+    def stop(self):
+        self.stopped = True
+
+
+if __name__ == "__main__":
+    print "Main Thread started"
+# Aufgabe 3
+#
+# Erstellen und starten Sie hier eine Instanz des DataThread und starten Sie den Webserver .
+
+# Einparken
+#
+# Erstellen und starten Sie hier eine Instanz des DrivingThread, um das Einparken zu ermoeglichen.
